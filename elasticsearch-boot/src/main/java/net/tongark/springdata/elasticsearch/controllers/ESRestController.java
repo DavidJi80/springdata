21278bf93b1f976fb0aced305c117e5a607ceaa3
David Ji <jz_1106@163.com>
2020-07-27 14:31:55 +0800

    ElasticSearch : Spring Boot

--- elasticsearch-boot/src/main/java/net/tongark/springdata/elasticsearch/controllers/ESRestController.java
+++ elasticsearch-boot/src/main/java/net/tongark/springdata/elasticsearch/controllers/ESRestController.java
@@ -4,9 +4,12 @@ import net.tongark.springdata.elasticsearch.entities.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/account")
 public class ESRestController {
 
     private final ElasticsearchRestTemplate restTemplate;
@@ -16,11 +19,11 @@ public class ESRestController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping("add")
-    public void add(){
+    @GetMapping("add/{id}/{name}")
+    public void add(@PathVariable long id,@PathVariable String name){
         Account account=new Account();
-        account.setId((long) 1);
-        account.setName("David's Account");
+        account.setId(id);
+        account.setName(name);
         restTemplate.save(account);
     }
 
